8a1b7cc1d4033347b37d874f846c34f916bcdc3f
deez <102386676+jj3623@users.noreply.github.com>
2023-05-15 13:57:36 -0500

    fuck

--- worker.js
+++ worker.js
@@ -2,6 +2,16 @@ onerror = function(err) {
   postMessage(err.message);
 }
 
-onmessage = function(e) {
-  postMessage(eval(e.data));
+onmessage = async function(e) {
+  let evaled = eval(e.data);
+  if(typeof evaled === 'object') {
+    if(evaled instanceof Promise) {
+      evaled = await evaled;
+    }
+    if(typeof evaled === 'object') {
+      evaled = JSON.stringify(evaled);
+    }
+  }
+  evaled = evaled.toString();
+  postMessage(evaled);
 }
