b9c540c1d81881bc554903112782673765fa781f
deez <102386676+jj3623@users.noreply.github.com>
2023-05-16 11:46:46 -0500

    rm -rf *;git push --force -m 'qwertyuiop'

--- worker.js
+++ worker.js
@@ -1,18 +1,17 @@
-onerror = function(e) {
-  e.preventDefault();
-  postMessage(e.message);
-}
-
 onmessage = async function(e) {
-  let evaled = eval(e.data);
-  if(typeof evaled === 'object') {
-    if(evaled instanceof Promise) {
-      evaled = await evaled;
-    }
+  try {
+    let evaled = eval(e.data);
     if(typeof evaled === 'object') {
-      evaled = JSON.stringify(evaled);
+      if(evaled instanceof Promise) {
+        evaled = await evaled;
+      }
+      if(typeof evaled === 'object') {
+        evaled = JSON.stringify(evaled);
+      }
     }
+    evaled = evaled.toString();
+    postMessage(evaled);
+  } catch(err) {
+    postMessage(err);
   }
-  evaled = evaled.toString();
-  postMessage(evaled);
 }
